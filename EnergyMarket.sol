45213bb344c4cc4d7926edb3719a2ff36fbe9c52
vicodin1123 <vicodin1123@gmail.com>
2017-03-13 00:17:50 +0800

    init

---
+++ EnergyMarket.sol
@@ -0,0 +1,53 @@
+pragma solidity ^0.4.2;
+
+contract SupplierContract 
+{
+	enum Status { OnTarget, LowerThanTarget, HigherThanTarget }
+
+	struct Supplier {		
+		address owner;
+		uint deposited;
+		uint fineRate;
+		uint target;
+		uint allowedRange;
+		uint pendingPayment;	
+	}
+
+	struct Consumer {
+		uint deposited;
+		uint fineRate;
+		uint target;
+		uint allowedRange;	
+		uint pendingPayment;	
+	}
+
+	// Contract is owned by a supplier
+	Supplier supplier;
+
+	// Addresse that is supplier or consumer;
+	mapping (address => bool) registered;
+
+	mapping (address => Consumer) consumerList;
+	
+	function SupplierContract(supplierFineRate, supplierTarget, supplierAllowedRange);
+
+	// Supplier add consumers to the contract
+	function newConsumer(fineRate, target, allowedRange) onlyOwner;
+
+	// Set status of supplier/cosumer according to the reported rate
+	function reportGenerated() onlyOwner;
+	function reportConsumed() onlyConsumer;
+
+	// Calculate fines according to status
+	function calculateFine() internal;
+
+	// Deposit/withdraw for supplier/cosumer
+	function deposit() onlyRegistered;
+	function withdraw() onlyRegistered;
+}
+
+contract EnergyMarket {
+	address[] contractList;
+
+	function newContract(uint supplierFineRate, uint supplierTarget, uint supplierSafeRange);
+}
